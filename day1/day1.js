56cf981dd3ca2cbbc1916cae5b425f91ac327f8f
atif mansuri <mansuriatif78@gmail.com>
2026-08-24 23:23:26 +0530

    add day1.js and day1.html files

---
+++ day1/day1.js
@@ -0,0 +1,29 @@
+for(let i = 0; i < 501; i++) {
+    console.log("kay re bhadu")
+}
+
+// start from 1 and end at 22
+
+for(let num = 1; num<23; num++) {
+    console.log(num)
+}
+
+// -------------------------------------------
+
+// start from 50 and end at 67
+
+for(let numTwo = 50; numTwo<68; numTwo++) {
+    console.log(numTwo)
+}
+
+// -------------------------------------------
+
+// start from 200 and end at 100
+
+for(let i = 200; i>99; i--) {
+    console.log(i)
+}
+
+// -------------------------------------------
+
+
