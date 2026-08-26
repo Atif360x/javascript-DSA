ac543f1dd40e669a640592b32d6952aae2f4ba6e
atif mansuri <mansuriatif78@gmail.com>
2026-08-26 12:03:44 +0530

    enable star pattern output and correct prompt usage

--- dayTwo/app.js
+++ dayTwo/app.js
@@ -5,41 +5,41 @@ let prompt = require('prompt-sync')();
 
 // let n = prompt("enter your name")
 
-// process.stdout.write(n)
+process.stdout.write(n)
 
-// -- patern programing
+// // -- patern programing
 
 // let n = prompt("enter the amount of stars you want")
 
-// for(let i=1; i <= n; i++){
-//     process.stdout.write("* ")
-// }
-// console.log()
+for(let i=1; i <= n; i++){
+    process.stdout.write("* ")
+}
+console.log()
  
-// for(let i=1; i <= n; i++){
-//     process.stdout.write("* ")
-// }
-// console.log()
+for(let i=1; i <= n; i++){
+    process.stdout.write("* ")
+}
+console.log()
  
-// for(let i=1; i <= n; i++){
-//     process.stdout.write("* ")
-// }
-// console.log()
+for(let i=1; i <= n; i++){
+    process.stdout.write("* ")
+}
+console.log()
  
-// for(let i=1; i <= n; i++){
-//     process.stdout.write("* ")
-// }
-// console.log()
+for(let i=1; i <= n; i++){
+    process.stdout.write("* ")
+}
+console.log()
  
-// for(let i=1; i <= n; i++){
-//     process.stdout.write("* ")
-// }
-// console.log()
+for(let i=1; i <= n; i++){
+    process.stdout.write("* ")
+}
+console.log()
  
-// for(let i=1; i <= n; i++){
-//     process.stdout.write("* ")
-// }
-// console.log()
+for(let i=1; i <= n; i++){
+    process.stdout.write("* ")
+}
+console.log()
  
 // * * * * * *
 // * * * * * *
@@ -48,11 +48,61 @@ let prompt = require('prompt-sync')();
 // * * * * * *
 // * * * * * *
 
-let n = prompt("enter the amount of stars you want")
+
+// // -------------------------------------------
+// // Q.1 right angle trangle
+
+let n = Number(prompt("enter the amount of stars you want"))
 
 for(let i=1; i<=n; i++) {
  for(let j=1; j<=i; j++) {
         process.stdout.write("* ")
  }
  console.log()
+}
+
+// // Q.1B 
+
+for(let i=1; i<=n; i++) {
+ for(let j=1; j<=i; j++) {
+        process.stdout.write(j+" ")
+ }
+ console.log()
+}
+
+// // Q.1C
+
+for(let i=1; i<=n; i++) {
+       let ascii = 65;
+ for(let j=1; j<=i; j++) {
+        process.stdout.write(String.fromCharCode(ascii)+" ")
+        ascii++
+ }
+ console.log()
+}
+
+// //----------------------------------------------------
+// // Q.2 inverted right angle ftrangle
+
+for(let i=1; i<=n; i++){
+       for(let j=1; j<=n-i+1; j++){
+              process.stdout.write("* ")
+       }
+       console.log()
+}
+
+
+// //------------------------------------------------
+
+// // Q.3 mirror right angle triangle
+
+for(let i=1; i<=n; i++){
+       for(let j=1; j<=n-i+1; j++){
+              process.stdout.write("  ")
+       }
+       for(let j=1; j<=i; j++){
+              process.stdout.write("* ")
+       }
+       console.log()
+       
 }
